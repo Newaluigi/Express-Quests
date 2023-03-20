f8fb14f9f4339d6e845e6cac1cc68bb45dc750e2
Newaluigi <alexandre.levillain@hotmail.com>
2023-03-20 11:26:40 +0100

    quest 03

--- movieHandlers.js
+++ movieHandlers.js
@@ -46,7 +46,6 @@ const getMovies = (req, res) => {
 
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
-
   database
     .query("select * from movies where id = ?", [id])
     .then(([movies]) => {
@@ -62,7 +61,24 @@ const getMovieById = (req, res) => {
     });
 };
 
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+  database
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the movie");
+    });
+};
+
 module.exports = {
   getMovies,
   getMovieById,
+  postMovie,
 };
